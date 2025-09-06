a4d5602bb8dd59fa6be939283bc5fbdf2b72dadd
saraieryo <rsa71010@gmail.com>
2025-09-06 21:53:23 +0900

    繋ぎ込みのテンプレ追加

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,22 +1,25 @@
-// app/page.jsx (HomePage.jsx)
-
 import React from "react";
 import ResultCard from "./components/ResultCard";
 import Link from "next/link";
-
 import data from "./data.json";
+
+// Import the HomeComponent to display the user dashboard
+import HomeComponent from "./components/Home";
 
 const HomePage = () => {
   const searchResults = data.results;
 
   return (
     <div className="container mx-auto p-4 mb-10">
-      <h1 className="text-2xl font-bold mb-6">あなたへのおすすめ</h1>
+      {/* ユーザーダッシュボードの表示 */}
+      <HomeComponent />
+
+      {/* 既存の検索結果セクション */}
+      <h1 className="text-2xl font-bold mb-6 mt-10">あなたへのおすすめ</h1>
       {searchResults.length > 0 ? (
         searchResults.map((result, index) => (
           <Link
             key={index}
-            // `href`にテンプレートリテラルを使ってidを組み込む
             href={`/detail/${result.id}`}
             passHref
           >
