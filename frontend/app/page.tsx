889f19635d895cdd9be1685e0a8afbc5257778e5
saraieryo <rsa71010@gmail.com>
2025-09-06 20:20:39 +0900

    ホームページの実装、一覧から詳細ページへの遷移を実装

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,33 +1,37 @@
+// app/page.jsx (HomePage.jsx)
+
+import React from "react";
+import ResultCard from "./components/ResultCard";
 import Link from "next/link";
 
-export default function Home() {
+import data from "./data.json";
+
+const HomePage = () => {
+  const searchResults = data.results;
+
   return (
-    <div>
-      <div>
-        <Link href="/create">
-          <button>会話デッキ作成</button>
-        </Link>
-      </div>
-      <div>
-        <Link href="/list">
-          <button>デッキ一覧</button>
-        </Link>
-      </div>
-      <div>
-        <Link href="/detail">
-          <button>デッキ詳細</button>
-        </Link>
-      </div>
-      <div>
-        <Link href="/login">
-          <button>ログイン</button>
-        </Link>
-      </div>
-      <div>
-        <Link href="/signup">
-          <button>サインアップ</button>
-        </Link>
-      </div>
+    <div className="container mx-auto p-4 mb-10">
+      <h1 className="text-2xl font-bold mb-6">あなたへのおすすめ</h1>
+      {searchResults.length > 0 ? (
+        searchResults.map((result, index) => (
+          <Link
+            key={index}
+            // `href`にテンプレートリテラルを使ってidを組み込む
+            href={`/detail/${result.id}`}
+            passHref
+          >
+            <ResultCard
+              title={result.title}
+              url={result.url}
+              content={result.content}
+            />
+          </Link>
+        ))
+      ) : (
+        <p>該当する検索結果はありませんでした。</p>
+      )}
     </div>
   );
-}
+};
+
+export default HomePage;
