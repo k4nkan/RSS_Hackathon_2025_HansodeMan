fe72ca9b2d08e3bfdd6af4a3b889fad48ef9c96f
k4nkan <kanta.y.0126@gmail.com>
2025-09-06 16:52:05 +0900

    #14 ログインと新規登録画面の追加

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,12 +1,71 @@
 import Link from "next/link";
-export default function SignUpPage() {
+
+export default function LoginPage() {
   return (
-    <div>
-      <h1>新規登録画面</h1>
-      <div>
-        <Link href="/">
-          <button>ホーム</button>
-        </Link>
+    <div className="flex h-screen items-center justify-center bg-white">
+      <div className="w-[16rem]">
+        <div className="flex w-full gap-[0.5rem] justify-center items-center mb-[2rem] pr-[1rem]">
+          <div className="w-[4rem] h-[4rem]">
+            <img src="logo.png" alt="app-logo" />
+          </div>
+          <h2 className="font-bold text-3xl text-blue-800">News2talk</h2>
+        </div>
+
+        <form className="space-y-[1rem]">
+          <div>
+            <label
+              htmlFor="userId"
+              className="block text-sm font-medium text-gray-700"
+            >
+              ユーザーID
+            </label>
+            <input
+              type="text"
+              id="userId"
+              className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="ユーザーIDを入力"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              パスワード
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="パスワードを入力"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              パスワードの確認
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 w-full rounded-lg border border-gray-300 px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="パスワードを入力"
+            />
+          </div>
+
+          <Link href="/">
+            <button
+              type="submit"
+              className="w-full rounded-lg bg-blue-600 py-2 text-white font-semibold hover:bg-blue-700 transition-colors"
+            >
+              サインアップ
+            </button>
+          </Link>
+        </form>
       </div>
     </div>
   );
