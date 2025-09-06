430263720cb26b7dfd6992bc3c547f33f3b3f5a0
Ryuuichi <131429001+HagaRyuiti@users.noreply.github.com>
2025-09-06 18:50:27 +0900

    #24デプロイ成功

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
-from app import create_app
+from __init__ import create_app
 
 app = create_app()
+
+@app.route("/")
+def index():
+    return "server on!!!"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- backend/routes/user_routes.py
+++ backend/routes/user_routes.py
@@ -1,17 +1,10 @@
 from flask import Blueprint, request, jsonify
-from controllers.user_controller import fetch_user, add_user, fetch_all_users
+from controllers.user_controller import add_user
 
 user_bp = Blueprint("user", __name__, url_prefix="/users")
-
-@user_bp.route("/<user_id>", methods=["GET"])
-def get_user(user_id):
-    user = fetch_user(user_id)
-    if user:
-        return jsonify(user), 200
-    return jsonify({"error": "User not found"}), 404
 
 @user_bp.route("/create_user", methods=["POST"])
 def create_user():
     data = request.json
-    add_user(data)
-    return jsonify({"message": "User created"}), 201
+    new_user_id = add_user(data)
+    return jsonify({"message": "User created successfully", "userId": new_user_id}), 201
